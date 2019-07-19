04d5ef392066fb18a31215f1781b1809b64c26f9
Золотарев Дмитрий <zolotarevdmitrij@Mac-mini-tech.local>
2019-07-19 17:05:20 +0300

    task1208

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //Напишите тут ваши методы
+    public static void print(String s) { }
+    public static void print(Integer i) { }
+    public static void print(Boolean b) { }
+    public static void print(Double d) { }
+    public static void print(Byte by) { }
+}
