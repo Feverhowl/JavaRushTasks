3c3e0cc122256bb875b55e1a85c0e8aae65f5a2d
Золотарев Дмитрий <zolotarevdmitrij@Mac-mini-tech.local>
2019-07-19 17:03:20 +0300

    task1207

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 2;
+        Integer j = 3;
+        print(i);
+        print(j);
+    }
+
+    //Напишите тут ваши методы
+    public static void print(int i) {
+        System.out.println(i);
+    }
+    public static void print(Integer j) {
+        System.out.println(j);
+    }
+}
